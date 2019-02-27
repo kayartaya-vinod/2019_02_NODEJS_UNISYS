1a81b4b0ce03f9be534c2f44bfc5538479276980
kayartaya.vinod <kayartaya.vinod@gmail.com>
2019-02-27 12:07:57 +0530

    🇮🇳

--- workspace/test/contact-service-async.spec.js
+++ workspace/test/contact-service-async.spec.js
@@ -8,7 +8,7 @@ describe('Testing Async version of ContactService', function () {
 
     let service;
 
-    beforeEach(function(){
+    beforeEach(function () {
         let contacts = [{
             "id": 1,
             "name": "Billi Ledwich",
@@ -34,20 +34,49 @@ describe('Testing Async version of ContactService', function () {
             "phone": "4832249761",
             "city": "Guocun"
         }];
-        
+
         fs.writeFileSync(filename, JSON.stringify(contacts));
         service = new ContactService();
     });
 
-    it('should get contact for id 1', function(done){
+    it('should get contact for id 1', function (done) {
 
-        service.getById(1, function(err, data) {
+        service.getById(1, function (err, data) {
             expect(data).to.be.a('object')
                 .to.have.property('name')
                 .to.equal('Billi Ledwich');
 
             done();
         })
+    });
+
+    it('should add new contact', function (finished) {
+        let c = {};
+        c.name = 'Vinod';
+        c.email = 'vinod@vinod.co';
+        c.phone = '9731424784';
+        c.city = 'Bangalore'
+
+        service.addNewContact(c, function (err, data) {
+            expect(err).to.equal(null);
+            expect(data).to.be.a('object')
+                .to.have.property('id')
+                .to.equal(5);
+
+            finished();
+
+        });
+    });
+
+    it('shuld not add a contact without name', function (done) {
+
+        let c = {};
+        service.addNewContact(c, (err, data) => {
+            expect(err).to.be.a('object');
+            expect(data).to.equal(undefined);
+            expect(err).to.have.property('code').to.equal(1003);
+            done();
+        });
     });
 
 });
